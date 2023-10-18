0fd7df43ebb2e819a350a503dad607948cc8b347
John Carmack <johncarmack@me.com>
2023-10-18 16:56:05 -0700

    Update input field styles and add responsive font size

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -13,13 +13,13 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
     return (
       <div
         className={cn(
-          "flex h-10 rounded-md border border-input px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+          "flex h-10 rounded-md border border-input px-3 py-2 ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
           className,
           props.disabled ? "bg-muted opacity-50" : "bg-background",
         )}
       >
         <input
-          className="w-full disabled:cursor-not-allowed disabled:bg-muted disabled:opacity-50"
+          className="w-full text-[16px] disabled:cursor-not-allowed disabled:bg-muted disabled:opacity-50 md:text-sm"
           type={type}
           ref={ref}
           {...props}
