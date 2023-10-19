e579d8db8b96cb280323c6f27dcd0f5aaab227a5
John Carmack <johncarmack@me.com>
2023-10-18 17:01:52 -0700

    Remove "turbo" from eslint config extends array

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,26 +2,14 @@
  * @type {import('eslint').Linter.Config}
  */
 module.exports = {
-  extends: ["next", "next/core-web-vitals", "turbo", "prettier"],
+  extends: ["next", "next/core-web-vitals"],
   ignorePatterns: ["node_modules", "dist"],
-  plugins: ["unused-imports"],
   settings: {
     next: {
       rootDir: ["web/*", "packages/*/"],
     },
   },
   rules: {
-    "no-unused-vars": "off",
-    "unused-imports/no-unused-imports": "error",
-    "unused-imports/no-unused-vars": [
-      "warn",
-      {
-        vars: "all",
-        varsIgnorePattern: "^_",
-        args: "after-used",
-        argsIgnorePattern: "^_",
-      },
-    ],
     "@next/next/no-html-link-for-pages": "off",
   },
 };
